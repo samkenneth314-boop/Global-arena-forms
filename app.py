ad0803fe175f145ad797d1da5190e0ba470cdff1
samkenneth314-boop <samkenneth314@gmail.com>
2025-10-08 09:31:50 +0000

    first commit

--- app.py
+++ app.py
@@ -262,7 +262,4 @@ def admin_logout():
 
 # ---------- Run ----------
 if __name__ == '__main__':
-    app.run(debug=True)
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000)) #Get the port from environment variable or use 5000 for local
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0',debug=True)
